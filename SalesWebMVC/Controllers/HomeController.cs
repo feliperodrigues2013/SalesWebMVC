1bf8822a76135483283445eee785f356d5665928
= <=>
2020-04-23 09:02:16 -0300

    CRUD scaffolding

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
+
 
 namespace SalesWebMVC.Controllers {
     public class HomeController : Controller {
